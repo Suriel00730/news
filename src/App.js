cf2c87a46d47ae61ddefa4d97ee74abeb14bed55
Suriel00730 <suriel00730is@gmail.com>
2022-04-02 23:09:17 -0400

    project ready

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
+
+function App() {
+
+  // Definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=9256941051324d39961d4b46f778025d`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria]);
+
+  return (
+    <>
+      <Header
+        titulo='Buscador de noticias'
+      />
+
+      <div className='container white'>
+        <Formulario
+          guardarCategoria={guardarCategoria}
+        />
+
+        <ListadoNoticias
+          noticias={noticias}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
